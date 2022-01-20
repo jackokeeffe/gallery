38c2784a1515b2f6a6f45f9fb400a10e1ae2c6e4
Jack O'Keeffe <55100016+jackokeeffe@users.noreply.github.com>
2022-01-20 16:40:22 -0500

    Update load.js

--- load.js
+++ load.js
@@ -4,7 +4,7 @@ var landscape = [4, 5, 7, 10, 13, 15, 16, ]
 function loadImages() {
     while (imageLoadCount <= 20) {
         var image = document.createElement("img");
-        image.src = "images/" + imageLoadCount + ".JPG";
+        image.src = "images/" + imageLoadCount + ".jpg";
 
         image.width = "20%";
 
