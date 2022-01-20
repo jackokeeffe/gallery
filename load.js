565e060097ed8a1e11bf219e49d89a56e7647f26
Jack O'Keeffe <55100016+jackokeeffe@users.noreply.github.com>
2022-01-20 16:32:20 -0500

    Add files via upload

---
+++ load.js
@@ -0,0 +1,21 @@
+var imageLoadCount = 0;
+var landscape = [4, 5, 7, 10, 13, 15, 16, ]
+
+function loadImages() {
+    while (imageLoadCount <= 20) {
+        var image = document.createElement("img");
+        image.src = "images/" + imageLoadCount + ".JPG";
+
+        image.width = "20%";
+
+        if (landscape.includes(imageLoadCount)) {
+            console.log(imageLoadCount)
+            image.id = "landscape"
+        }
+
+        document.getElementById("images").appendChild(image);
+        imageLoadCount++;
+    }
+}
+
+loadImages();
